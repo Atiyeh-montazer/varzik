c17231c043378acc50a05ccb671862f8d0f65d64
Atiyeh-montazer <sadailmon.am@gmail.com>
2024-09-09 19:17:04 -0700

    added carocel

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,9 +12,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.className} h-screen max-w-[430px] min-w-[350px]  mx-auto  flex flex-col justify-between`}>
         <Header />
-        <main className="h-full bgImage">
-          {children}
-
+        <main className="h-full  bg-[url(../../public/images/45562.jpg)] bg-no-repeat bg-right-top bg-cover" >
+          <div className="bg-gray-500 bg-opacity-75 h-full">
+            {children}
+          </div>
         </main>
         <Footer />
       </body>
