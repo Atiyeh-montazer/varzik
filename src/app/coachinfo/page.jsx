9c1bab829c61d78571d6a265a85c31025f7cda46
Atiyeh-montazer <sadailmon.am@gmail.com>
2024-09-14 13:56:45 -0700

    coachinfo

--- src/app/coachinfo/page.jsx
+++ src/app/coachinfo/page.jsx
@@ -8,7 +8,7 @@ function Coachinfo() {
 
             </div>
             <div className='flex justify-center mt-[422px] '>
-                <Link href="/coachlist">
+                <Link href="/coach-prof">
                     <button className='hover:bg-pink-700 mt-[6rem] w-32 h-11 border  border-x-4 rounded-full mr-5 flex justify-center items-center text-xl'>بازگشت</button>
                 </Link>
 
