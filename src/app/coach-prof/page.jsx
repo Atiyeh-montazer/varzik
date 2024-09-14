b5bad41a94cfd4727feda13b423a01046d962ff3
Atiyeh-montazer <sadailmon.am@gmail.com>
2024-09-14 14:09:37 -0700

    robat

--- src/app/coach-prof/page.jsx
+++ src/app/coach-prof/page.jsx
@@ -33,13 +33,26 @@ function Coachprof() {
                     </div>
                 </div>
                 <div class=" bg-[#c6d8d3] border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700  mt-5">
-                    <div class="flex justify-end px-4 pt-4 ">
 
-                    </div>
-
-                    <div class="flex flex-col items-center pb-5 ">
+                    <div class="flex  flex-col items-center pb-5 ">
 
                         <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">فهرست شاگردان فعال</h5>
+
+                        <div class="flex mt-4 mb-2 md:mt-6">
+
+                            <Link href="/coachinfo">
+                                <button className='hover:bg-pink-700  mt-1  w-32 h-8 border  border-x-4 rounded-full mr-0 flex justify-center items-center text-sm'>مشاهده وضعیت</button>
+                            </Link>
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <div class=" bg-[#c6d8d3] border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700  mt-5">
+                    <div class="flex flex-col items-center pb-5 ">
+
+                        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">آموزش ربات</h5>
 
                         <div class="flex mt-4 mb-2 md:mt-6">
 
@@ -50,10 +63,10 @@ function Coachprof() {
 
                     </div>
                 </div>
-
-
-
             </div>
+
+
+
         </>
     )
 }
