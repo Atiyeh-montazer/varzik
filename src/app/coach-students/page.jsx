a72c4a1fe7803b3300daa15c4dd85b81948cb6ba
atiyeh <sadailmon.am@gmail.com>
2024-09-16 10:04:48 +0330

    prof

--- src/app/coach-students/page.jsx
+++ src/app/coach-students/page.jsx
@@ -45,7 +45,7 @@ function CoachStudents() {
                 </div>
             </div>
             <div className='flex justify-center mt-48 '>
-                <Link href="/mainPage">
+                <Link href="/coach-prof">
                     <button className='hover:bg-pink-700 mt-[6rem] w-32 h-11 border  border-x-4 rounded-full mr-5 flex justify-center items-center text-xl'>بازگشت</button>
                 </Link>
 
