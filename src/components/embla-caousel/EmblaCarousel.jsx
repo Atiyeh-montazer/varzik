1c6092dcd784d7f935bf0734ba68e814c8a3648e
wildonion <ea_pain@yahoo.com>
2024-10-07 21:33:10 +0330

    fix the title props bug

--- src/components/embla-caousel/EmblaCarousel.jsx
+++ src/components/embla-caousel/EmblaCarousel.jsx
@@ -7,14 +7,11 @@ import {
 } from './EmblaCarouselArrowButtons'
 import useEmblaCarousel from 'embla-carousel-react'
 import Image from 'next/image'
-import { Changa } from 'next/font/google'
 
-const EmblaCarousel = (props) => {
-
-
-  const {title, slides, options } = props
+const EmblaCarousel = ({ title, slides, options }) => {  // Destructure props here
   const [emblaRef, emblaApi] = useEmblaCarousel(options)
-console.log(title)
+  console.log('Title in EmblaCarousel:', title)  // Check if the title is being logged
+
   const { selectedIndex, scrollSnaps, onDotButtonClick } =
     useDotButton(emblaApi)
 
@@ -22,39 +19,17 @@ console.log(title)
     prevBtnDisabled,
     nextBtnDisabled,
     onPrevButtonClick,
-
-
-
     onNextButtonClick
   } = usePrevNextButtons(emblaApi)
 
   return (
     <section className="embla">
-      {/* <div className='m-4 text-right text-white text-4xl  drop-shadow-lg'>
-           <h1>برترین ها</h1> 
-      </div> */}
-  
-      {/* <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
-
-          
-          {slides.map((slide) => (
-            <div className="embla__slide" key={slide.id}>
-              
-              <Image src={slide.src} width={1000} height={50} className='rounded-xl mt-4 border border-slate-400' />
-            </div>
-          ))}
-        </div>
-      </div> */}
-
-
-
-
-
-
+      {/* Title section */}
       <div className='m-4 mt-8 mb-2 text-right text-white text-4xl  drop-shadow-lg'>
-           <h1>{props.title}</h1> 
+        <h1>{title}</h1> {/* Use 'title' directly here */}
       </div>
+
+      {/* Carousel */}
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
           {slides.map((slide) => (
@@ -65,13 +40,14 @@ console.log(title)
         </div>
       </div>
 
-      {/* <div className="embla__controls">
+      {/* Navigation (if needed) */}
+      {/* 
+      <div className="embla__controls">
         <div className="embla__buttons">
           <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
           <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
         </div>
-
-       <div className="embla__dots">
+        <div className="embla__dots">
           {scrollSnaps.map((_, index) => (
             <DotButton
               key={index}
@@ -82,11 +58,9 @@ console.log(title)
             />
           ))}
         </div>
-
-
-      </div> */}
+      </div> 
+      */}
     </section>
-
   )
 }
 
